3b66fe401cc9fca94ecc6c063466dafa67845b90
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-02-23 22:27:49 +0200

    upd1

--- src/scriptjs/api.js
+++ src/scriptjs/api.js
@@ -1,2 +1,13 @@
 import axios from "axios";
-const ENDPOINT = ""
+const ENDPOINT = 'https://pixabay.com/api/';
+const KEY = '33796051-1e422d07d8c9fc912bc2a7eb4';
+const options =
+  'image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
+
+async function searchImage(name) {
+  const URL = `${ENDPOINT}?${KEY}&q=${name}&${options}`;
+  // const response = wait.axios.get(URL);
+  return wait.axios.get(URL);
+}
+
+// export default searchImage;
